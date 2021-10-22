f34d9287d8b6582912ebfb21b85f8daf489f5c58
Guilherme Nunes Lopes <guilherme.lopes13@hotmail.com>
2021-10-21 21:54:48 -0300

    criando ip publico e subrede

--- main.tf
+++ main.tf
@@ -29,7 +29,27 @@ resource "azurerm_virtual_network" "teste-serasa" {
   resource_group_name = azurerm_resource_group.teste-serasa.name
   #vinculando a rede a região de West US 2
   location = azurerm_resource_group.teste-serasa.location
-  address_space       = ["10.20.10.0/24"]
+  address_space       = ["10.20.0.0/16"]
 }
 
-#Criando o a máquina
+#Criando subnet interna
+
+resource "azurerm_subnet" "interno" {
+  name                 = "interno-serasa"
+  resource_group_name  = azurerm_resource_group.teste-serasa.name
+  virtual_network_name = azurerm_virtual_network.teste-serasa.name
+  address_prefixes     = ["10.20.1.0/24"]
+}
+
+#Criando um ip publico
+
+resource "azurerm_public_ip" "example" {
+  name                = "Ip_Publico_SeExperian"
+  resource_group_name = azurerm_resource_group.teste-serasa.name
+  location            = azurerm_resource_group.teste-serasa.location
+  allocation_method   = "Static"
+
+  tags = {
+    environment = "Aplicação Go"
+  }
+}
